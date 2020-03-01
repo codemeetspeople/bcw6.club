8ab2516ac5177df7b168b801768b7aca3d4b87fc
caiman <me@caiman.dev>
2020-03-01 17:59:01 +0200

    [master] Add Jinja2 templates

--- src/core/settings_local.py
+++ src/core/settings_local.py
@@ -6,10 +6,56 @@ DATABASES = {
         'USER': 'shop',
         'NAME': 'shop',
         'PASSWORD': 'shop',
-        # 'TEST': {
-        #     'NAME': 'mytestdatabase',
-        # },
     },
 }
+
+TEMPLATES = [
+    {
+        "BACKEND": "django_jinja.backend.Jinja2",
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "match_extension": ".tpl",
+            "match_regex": r"^(?!admin/).*",
+            "app_dirname": "templates",
+            "undefined": None,
+            "newstyle_gettext": True,
+            "extensions": [
+                "jinja2.ext.do",
+                "jinja2.ext.loopcontrols",
+                "jinja2.ext.with_",
+                "jinja2.ext.i18n",
+                "jinja2.ext.autoescape",
+                "django_jinja.builtins.extensions.CsrfExtension",
+                "django_jinja.builtins.extensions.CacheExtension",
+                "django_jinja.builtins.extensions.DebugExtension",
+                "django_jinja.builtins.extensions.TimezoneExtension",
+                "django_jinja.builtins.extensions.UrlsExtension",
+                "django_jinja.builtins.extensions.StaticFilesExtension",
+                "django_jinja.builtins.extensions.DjangoFiltersExtension",
+            ],
+            "bytecode_cache": {
+                "name": "default",
+                "backend": "django_jinja.cache.BytecodeCache",
+                "enabled": False,
+            },
+            "autoescape": True,
+            "auto_reload": DEBUG,
+            "translation_engine": "django.utils.translation",
+        }
+    },
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
 
 LANGUAGE_CODE = 'ru-ru'
---
+++ src/core/templates/core/base.tpl
@@ -0,0 +1,10 @@
+<html>
+    <!DOCTYPE html>
+    <head>
+        <meta charset="utf-8">
+        <title>Home</title>
+    </head>
+    <body> 
+        <h1>Hello, Django!</h1>
+    </body>
+</html>
